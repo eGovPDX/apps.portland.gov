6fdeb9268dde75d976c5159028297574fa4af3ca
Ronnie Mosley <ronnie.j.mosley@gmail.com>
2025-07-02 18:02:39 +0000

    populate list of events

--- apps/city-calendar/src/app/events/page.tsx
+++ apps/city-calendar/src/app/events/page.tsx
@@ -1,7 +1,25 @@
-export default () => {
+import { getEvents } from "@/data/event";
+export default async () => {
+  const events = await getEvents();
   return (
     <div>
       <h2>Events</h2>
+      { events.length > 0 ? (
+        <ul>
+          {events.map((event) => (
+            <li key={event.id} data-key={event.id}>
+              <h3>{event.title}</h3>
+              <p>
+                <strong>When:</strong> {event.start.toLocaleString()} - {event.end.toLocaleString()}<br />
+                <strong>Where:</strong> {event.location}<br />
+                <strong>Description:</strong> {event.description}
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No events found.</p>
+      )}
     </div>
   );
 };
